5d0553af59bc827080b6cb6a2cd0ff6df961ef29
TheBigRedDog <77410051+TheBigRedDog@users.noreply.github.com>
2021-01-27 11:57:28 -0800

    Update page.py

--- template/page.py
+++ template/page.py
@@ -11,6 +11,10 @@ class Page:
         if self.num_records >= 4096:
             return False
         # Check if Full, so if bytes go over 4096 or 4 KB
+        # Question: According to the milestone 1 pdf "all columns are 64-bit integers in this implementation".
+        # I'm going to assume that means the actual info put in will be in the form of a 64-bit integer.
+        # In that case, wouldn't each record take up 8 bytes in a particular byte array?
+        
         return True
 
     def write(self, value):
